fe6a902650a064d8848a1124c3536c4c8dde6aa8
zhangfan <fanstudio@qq.com>
2019-03-03 10:54:44 +0800

    添加mybatis-plus代码生成器示例

---
+++ mp-codegenerator/src/main/java/com/fanstudio/mpcodegenerator/MpCodegeneratorApplication.java
@@ -0,0 +1,12 @@
+package com.fanstudio.mpcodegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MpCodegeneratorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MpCodegeneratorApplication.class, args);
+    }
+}
---
+++ mp-codegenerator/src/test/java/com/fanstudio/mpcodegenerator/MpCodegeneratorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fanstudio.mpcodegenerator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MpCodegeneratorApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
